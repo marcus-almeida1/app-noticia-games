abb1762d12d21d05d810b6042e7c777f81b12165
MARCUS VINICIUS VELHO DE ALMEIDA <marcus.almeida1@alunos.sc.senac.br>
2025-06-13 21:30:26 -0300

    feat: criado o modelo da home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButtons, IonButton, IonItem, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { thumbsUpOutline, thumbsDownOutline } from 'ionicons/icons'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonIcon, IonItem, IonButton, IonButtons, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({thumbsUpOutline, thumbsDownOutline})
+  }
+
+  curtido = false;
+  naoCurtido = false;
+
+  curtir() {
+    this.curtido = !this.curtido;
+    
+    if (this.curtido) {
+      this.naoCurtido = false;
+    }
+  }
+
+  naoCurtir() {
+    this.naoCurtido = !this.naoCurtido;
+
+    if (this.naoCurtido) {
+      this.curtido = false;
+    }
+  }
 }
