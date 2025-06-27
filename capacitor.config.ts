03014af7bc81b1fa25730309e37849d069c72d4b
MARCUS VINICIUS VELHO DE ALMEIDA <marcus.almeida1@alunos.sc.senac.br>
2025-06-27 19:23:51 -0300

    feat: adicionado icones, splash-sreen e nome customizado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'noticia-games',
+  appName: 'GG News',
   webDir: 'www'
 };
 
